72d279624b2593f6430c1e19f06b41752a0c05d2
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-09 17:26:48 +0100

    expandindo / formatando o componente field

--- App.tsx
+++ App.tsx
@@ -11,6 +11,15 @@ export default class App extends Component {
           <Text>Iniciando mines!</Text>
           <Text>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()} </Text>
           <Field />
+          <Field opened />
+          <Field opened nearMines={1} />
+          <Field opened nearMines={2} />
+          <Field opened nearMines={3} />
+          <Field opened nearMines={4} />
+          <Field opened nearMines={5} />
+          <Field opened nearMines={6} />
+          <Field opened nearMines={7} />
+          <Field opened nearMines={8} />
         </View>
       )
   }
@@ -20,6 +29,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
 });
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import params from '../params';
 
 export default props => {
+  const {mined, opened, nearMines} = props;
+
   const stylesField = [styles.field];
+  if (opened) stylesField.push(styles.opened);
   if (stylesField.length === 1) stylesField.push(styles.regular);
 
-  return <View style={stylesField}></View>;
+  let color = null;
+  if (nearMines > 0) {
+    if (nearMines === 1) color = '#2A28D7';
+    if (nearMines === 2) color = '#2B520F';
+    if (nearMines > 2 && nearMines < 6) color = '#F9060A';
+    if (nearMines >= 6) color = '#F221A9';
+  }
+
+  return (
+    <View style={stylesField}>
+      {!mined && opened && nearMines > 0 ? (
+        <Text style={[styles.label, {color: color}]}>{nearMines}</Text>
+      ) : (
+        false
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -17,9 +36,19 @@ const styles = StyleSheet.create({
   },
   regular: {
     backgroundColor: '#999',
-    borderLeftColor: '#ccc',
-    borderTopColor: '#ccc',
+    borderLeftColor: '#CCC',
+    borderTopColor: '#CCC',
     borderRightColor: '#333',
     borderBottomColor: '#333',
+  },
+  opened: {
+    backgroundColor: '#999',
+    borderColor: '#777',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: params.fontSize,
   },
 });
